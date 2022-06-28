a75730acbd69b952b36f85015e33f13f2379cc09
Katelyn Rakonjac King <katelynking@Katelyns-MacBook-Air.local>
2022-06-28 19:05:26 -0400

    working

--- server.js
+++ server.js
@@ -1,22 +1,53 @@
-const express = require('express');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const db = require("./db/db.json");
+const { v4: uuidv4 } = require("uuid");
 
-var app = express();
+const PORT = process.env.PORT || 3001;
 
-var PORT = process.env.PORT || 3001;
-
-app.use(clog);
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
+app.get("/api/notes", (req, res) => {
+    res.json(db);
+  });
 
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+)
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    newNote.id = uuidv4();
+    db.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(db), (err) => {
+      if (err) throw err;
+    });
+    res.send(db);
+  })
 
-app.listen(PORT, function () {
-    console.log(`App listening at http://localhost:${PORT} 🚀`)
-});
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+)
+
+app.delete("/api/notes/:id", (req, res) => {
+  db.forEach((note, i) => {
+    if (note.id === req.params.id) {
+      db.splice(i, 1);
+    }
+  })
+
+  fs.writeFile("db/db.json", JSON.stringify(db), (err) => {
+    if (err) throw err;
+  });
+  res.send(db);
+})
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
